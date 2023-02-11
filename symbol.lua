ebb607f2c209818e8c964f7077861a940fac5b57
GiacomoMaletto <malettogiacomo6@gmail.com>
2023-02-11 22:43:22 +0100

    Refactorized symbol.lua

--- symbol.lua
+++ symbol.lua
@@ -14,40 +14,238 @@ local S = {}
 S.mt = {}
 
 function S.new(s)
-    s = s or {}
-    if type(s) == "table" then setmetatable(s, S.mt) end
-    return s
+    if getmetatable(s) == S.mt then return s end
+    local result
+    if s == nil then result = {}
+    elseif type(s) == "number" or type(s) == "string" then result = {s}
+    elseif type(s) == "table" then result = s
+    else error() end
+    setmetatable(result, S.mt)
+    return result
 end
 
 setmetatable(S, {__call = function(self, s) return S.new(s) end})
 
+S.mt.__index = S
 
+function S.copy(e)
+    return S(deep_copy(e))
+end
 
+function S.num(e)
+    return e[1]
+end
 
+function S.var(e)
+    return e[1]
+end
 
+function S.op(e)
+    return e[1]
+end
 
+function S.fst(e)
+    return S(e[2])
+end
 
+function S.snd(e)
+    return S(e[3])
+end
 
-function S.der(e, v)
-    if S.is_empty(e) then error()
-    elseif S.is_num(e) then return 0
-    elseif S.is_var(e) then return v == e and 1 or 0
-    elseif S.is_add(e) then
-        return {"+", S.der(S.fst(e), v), S.der(S.snd(e), v)}
-    elseif S.is_sub(e) then
-        return {"-", S.der(S.fst(e), v), S.der(S.snd(e), v)}
-    elseif S.is_mul(e) then
-        local f = S.fst(e)
-        local g = S.snd(e)
-        local fp = S.der(f, v)
-        local gp = S.der(g, v)
-        return {"+", {"*", fp, g}, {"*", f, gp}}
-    elseif S.is_pow(e) then
-        local f = S.fst(e)
-        local n = S.snd(e)
-        local fp = S.der(f, v)
-        return {"*", n, {"*", {"^", f, n - 1}, fp}}
+-- empty: some operations are undefined on it (ex. evaluation)
+-- but it is useful because addition and multiplication are defined st
+-- S() + e = e
+-- S() * e = e
+-- so extended sums and products are easier to define
+function S.is_empty(e)
+    return type(e) == "table" and #e == 0
+end
+
+function S.is_num(e)
+    return #e == 1 and C.is_scalar(e[1])
+end
+
+function S.is_real(e)
+    return #e == 1 and C.is_real(e[1])
+end
+
+function S.is_cpx(e)
+    return #e == 1 and C.is_complex(e[1])
+end
+
+function S.is_var(e)
+    return #e == 1 and type(e[1]) == "string"
+end
+
+function S.is_add(e)
+    return #e == 3 and e[1] == "+"
+end
+
+function S.is_sub(e)
+    return #e == 3 and e[1] == "-"
+end
+
+function S.is_add_or_sub(e)
+    return e:is_add() or e:is_sub()
+end
+
+function S.is_mul(e)
+    return #e == 3 and e[1] == "*"
+end
+
+-- only power where the exponent is a non-negative integer are allowed
+function S.is_pow(e)
+    return #e == 3 and e[1] == "^"
+end
+
+function S.is_bin(e)
+    return e:is_add() or e:is_sub() or e:is_mul() or e:is_pow()
+end
+
+-- side effect: changes the metatable of its arguments
+function S.add(e, f)
+    e, f = S(e), S(f)
+    if e:is_empty() and f:is_empty() then return S()
+    elseif e:is_empty() and not f:is_empty() then return f
+    elseif not e:is_empty() and f:is_empty() then return e
+    else return S{"+", e, f} end
+end
+
+S.mt.__add = S.add
+
+-- side effect: changes the metatable of its arguments
+function S.sub(e, f)
+    e, f = S(e), S(f)
+    if e:is_empty() and f:is_empty() then return S()
+    elseif e:is_empty() and not f:is_empty() then return S{"*", S(-1), f}
+    elseif not e:is_empty() and f:is_empty() then return e
+    else return S{"-", e, f} end
+end
+
+S.mt.__sub = S.sub
+
+-- side effect: changes the metatable of its arguments
+function S.mul(e, f)
+    e, f = S(e), S(f)
+    if e:is_empty() and f:is_empty() then return S()
+    elseif e:is_empty() and not f:is_empty() then return f
+    elseif not e:is_empty() and f:is_empty() then return e
+    else return S{"*", e, f} end
+end
+
+S.mt.__mul = S.mul
+
+-- side effect: changes the metatable of its arguments
+function S.pow(e, n)
+    e, n = S(e), S(n)
+    if e:is_empty() then return S()
+    else return S{"^", e, n} end
+end
+
+S.mt.__pow = S.pow
+
+function S.bin(o, e, f)
+    if o == "+" then return e + f
+    elseif o == "-" then return e - f
+    elseif o == "*" then return e * f
+    elseif o == "^" then return e ^ f end
+end
+
+function S.to_string(e)
+    if e:is_empty() then return "()"
+    elseif e:is_num() then return tostring(e:num())
+    elseif e:is_var() then return e:var()
+    elseif e:is_add_or_sub() then
+        return "(" .. e:fst():to_string() .. " " .. e:op() .. " " .. e:snd():to_string() .. ")"
+    elseif e:is_mul() or e:is_pow() then
+        return "(" .. e:fst():to_string() .. e:op() .. e:snd():to_string() .. ")"
     end
+end
+
+S.mt.__tostring = S.to_string
+
+function S.eval(e, x)
+    if e:is_empty() then error()
+    elseif e:is_num() then return e:num()
+    elseif e:is_var() then return x[e:var()]
+    elseif e:is_add() then return e:fst():eval(x) + e:snd():eval(x)
+    elseif e:is_sub() then return e:fst():eval(x) - e:snd():eval(x)
+    elseif e:is_mul() then return e:fst():eval(x) * e:snd():eval(x)
+    elseif e:is_pow() then return e:fst():eval(x) ^ e:snd():eval(x) end
+end
+
+function S.vars(e)
+    local result = {}
+    local function helper(f)
+        if f:is_empty() or f:is_num() then
+        elseif f:is_var() then Set.insert(result, f:var())
+        elseif f:is_bin() then helper(f:fst()); helper(f:snd()) end
+    end
+    helper(e)
+    table.sort(result)
+    return result
+end
+
+function S.pow_to_mul(e)
+    if e:is_empty() then return S()
+    elseif e:is_num() then return S(e:num())
+    elseif e:is_var() then return S(e:var())
+    elseif e:is_pow() then
+        local b = e:fst():pow_to_mul()
+        local n = e:snd():num()
+        local result = S()
+        for i = 1, n do
+            result = result * b
+        end
+        return result
+    elseif e:is_bin() then
+        return S.bin(e:op(), e:fst():pow_to_mul(), e:snd():pow_to_mul())
+    end
+end
+
+-- can be made faster
+function S.cpx_to_real(e, t)
+    if e:is_empty() then return S(), S()
+    elseif e:is_real() then return S(e:num()), S(e:num())
+    elseif e:is_cpx() then return S(e:num()[1]), S(e:num()[2])
+    elseif e:is_var() then return S(t[e:var()][1]), S(t[e:var()][2])
+    elseif e:is_add() then
+        local r1, i1 = e:fst():cpx_to_real(t)
+        local r2, i2 = e:snd():cpx_to_real(t)
+        return r1 + r2, i1 + i2
+    elseif e:is_sub() then
+        local r1, i1 = e:fst():cpx_to_real(t)
+        local r2, i2 = e:snd():cpx_to_real(t)
+        return r1 - r2, i1 - i2
+    elseif e:is_mul() then
+        local r1, i1 = e:fst():cpx_to_real(t)
+        local r2, i2 = e:snd():cpx_to_real(t)
+        return r1*r2 - i1*i2, r1*i2 + r2*i1
+    elseif e:is_pow() then return e:pow_to_mul():cpx_to_real(t) end
+end
+
+function S.distr(e)
+    local function helper(f)
+        if f:is_empty() or f:is_num() or f:is_var() then return f, false
+        elseif f:is_mul() and f:fst():is_add_or_sub() then
+            local f1 = helper(f:fst():fst() * f:snd())
+            local f2 = helper(f:fst():snd() * f:snd())
+            return S.bin(f:fst():op(), f1, f2), true
+        elseif f:is_mul() and f:snd():is_add_or_sub() then
+            local f1 = helper(f:fst() * f:snd():fst())
+            local f2 = helper(f:fst() * f:snd():snd())
+            return S.bin(f:snd():op(), f1, f2), true
+        elseif f:is_bin() then
+            local fst, fst_b = helper(f:fst())
+            local snd, snd_b = helper(f:snd())
+            return S.bin(f:op(), fst, snd), fst_b or snd_b
+        end
+    end
+    local b = true
+    while b do
+        e, b = helper(e)
+    end
+    return e
 end
 
 function S.normal(e)
@@ -69,25 +267,25 @@ function S.normal(e)
 
     -- adds monomials
     local function insert(f)
-        if S.is_num(f) then
-            mons[#mons].scalar = mons[#mons].scalar * f
-        elseif S.is_var(f) then
-            mons[#mons][f] = mons[#mons][f] + 1
-        elseif S.is_mul(f) then
-            insert(S.fst(f))
-            insert(S.snd(f))
-        else error(f[1]) end
+        if f:is_num() then
+            mons[#mons].scalar = mons[#mons].scalar * f:num()
+        elseif f:is_var() then
+            mons[#mons][f:var()] = mons[#mons][f:var()] + 1
+        elseif f:is_mul() then
+            insert(f:fst())
+            insert(f:snd())
+        else error() end
     end
 
     -- searches for monomials
     local function scan(f)
-        if S.is_num(f) or S.is_var(f) or S.is_mul(f) then
+        if f:is_num() or f:is_var() or f:is_mul() then
             table.insert(mons, deep_copy(mon_default))
             insert(f)
-        elseif S.is_add(f) or S.is_sub(f) then
-            scan(S.fst(f))
-            scan(S.snd(f))
-        elseif S.is_pow(f) then error() end
+        elseif f:is_add_or_sub() then
+            scan(f:fst())
+            scan(f:snd())
+        elseif f:is_pow() then error() end
     end
 
     scan(simple)
@@ -99,7 +297,7 @@ function S.normal(e)
         for j, s in ipairs(summed) do
             local m_eq_s = true
             for _, v in ipairs(vars) do
-                if not (s[v] == m[v]) then m_eq_s = false; break end
+                if s[v] ~= m[v] then m_eq_s = false; break end
             end
             if m_eq_s then
                 s.scalar = s.scalar + m.scalar
@@ -119,8 +317,8 @@ function S.normal(e)
         else mon_s = S(s.scalar) end
         for _, v in ipairs(vars) do
             if s[v] == 0 then
-            elseif s[v] == 1 then mon_s = S.mul(mon_s, v)
-            elseif s[v] > 0 then mon_s = S.mul(mon_s, S.pow(v, s[v])) end
+            elseif s[v] == 1 then mon_s = mon_s * S(v)
+            elseif s[v] > 0 then mon_s = mon_s * S(v)^S(s[v]) end
         end
         result = result + mon_s
         ::continue::
@@ -129,182 +327,39 @@ function S.normal(e)
     return result
 end
 
-function S.distr(e)
-    local function helper(f)
-        if S.is_empty(f) or S.is_num(f) or S.is_var(f) then return f, false
-        elseif S.is_mul(f) and S.is_add_or_sub(S.snd(f)) then
-            local f1 = helper({"*", S.fst(f), S.fst(S.snd(f))})
-            local f2 = helper({"*", S.fst(f), S.snd(S.snd(f))})
-            return S({S.op(S.snd(f)), f1, f2}), true
-        elseif S.is_mul(f) and S.is_add_or_sub(S.fst(f)) then
-            return helper(S({"*", S.snd(f), S.fst(f)}))
-        elseif S.is_bin(f) then
-            local fst, fst_b = helper(S.fst(f))
-            local snd, snd_b = helper(S.snd(f))
-            return S({S.op(f), fst, snd}), fst_b or snd_b
-        end
-    end
-    local b = true
-    while b do
-        e, b = helper(e)
-    end
-    return e
-end
-
-function S.vars(e)
-    local result = {}
-    local function helper(f)
-        if S.is_empty(f) or S.is_num(f) then
-        elseif S.is_var(f) then Set.insert(result, f)
-        elseif S.is_bin(f) then helper(S.fst(f)); helper(S.snd(f)) end
-    end
-    helper(e)
-    return result
-end
-
-function S.to_string(e)
-    if S.is_empty(e) then return "()"
-    elseif S.is_num(e) then return tostring(e)
-    elseif S.is_var(e) then return e
-    elseif S.is_add(e) or S.is_sub(e) then
-        return "(" .. S.to_string(S.fst(e)) .. " " .. S.op(e) .. " " .. S.to_string(S.snd(e)) .. ")"
-    elseif S.is_mul(e) or S.is_pow(e) then
-        return "(" .. S.to_string(S.fst(e)) .. S.op(e) .. S.to_string(S.snd(e)) .. ")"
+-- v can be either a string or a symbolic variable
+function S.der(e, v)
+    if e:is_empty() then error()
+    elseif e:is_num() then return S(0)
+    elseif e:is_var() then return S(v):var() == e:var() and S(1) or S(0)
+    elseif e:is_add() then
+        return e:fst():der(v) + e:snd():der(v)
+    elseif e:is_sub() then
+        return e:fst():der(v) - e:snd():der(v)
+    elseif e:is_mul() then
+        local f = e:fst()
+        local g = e:snd()
+        local fp = f:der(v)
+        local gp = g:der(v)
+        return fp * g + f * gp
+    elseif e:is_pow() then
+        local f = e:fst()
+        local n = e:snd():num()
+        local fp = f:der(v)
+        return n * f^(n-1) * fp
     end
 end
 
-S.mt.__tostring = S.to_string
 
-function S.pow_to_mul(e)
-    if S.is_empty(e) then return S()
-    elseif S.is_num(e) or S.is_var(e) then return S(e)
-    elseif S.is_pow(e) then
-        local b = S.fst(e)
-        local n = S.snd(e)
-        if n == 1 then return S(b)
-        else return S({"*", b, S.pow_to_mul({"^", b, n - 1})}) end
-    elseif S.is_bin(e) then
-        return S({S.op(e), S.pow_to_mul(S.fst(e)), S.pow_to_mul(S.snd(e))})
-    end
-end
-
-function S.cpx_to_real(e, t)
-    if S.is_empty(e) then return {}
-    elseif S.is_real(e) then return e, e
-    elseif S.is_cpx(e) then return e[1], e[2]
-    elseif S.is_var(e) then return t[e][1], t[e][2]
-    elseif S.is_add(e) then
-        local r1, i1 = S.cpx_to_real(S.fst(e), t)
-        local r2, i2 = S.cpx_to_real(S.snd(e), t)
-        return {"+", r1, r2}, {"+", i1, i2}
-    elseif S.is_sub(e) then
-        local r1, i1 = S.cpx_to_real(S.fst(e), t)
-        local r2, i2 = S.cpx_to_real(S.snd(e), t)
-        return {"-", r1, r2}, {"-", i1, i2}
-    elseif S.is_mul(e) then
-        local r1, i1 = S.cpx_to_real(S.fst(e), t)
-        local r2, i2 = S.cpx_to_real(S.snd(e), t)
-        return {"-", {"*", r1, r2}, {"*", i1, i2}}, {"+", {"*", r1, i2}, {"*", r2, i1}}
-    elseif S.is_pow(e) then return S.cpx_to_real(S.pow_to_mul(e), t) end
-end
-
-function S.eval(e, x)
-    if S.is_empty(e) then error()
-    elseif S.is_num(e) then return e
-    elseif S.is_var(e) then return x[e]
-    elseif S.is_add(e) then return S.eval(S.fst(e), x) + S.eval(S.snd(e), x)
-    elseif S.is_sub(e) then return S.eval(S.fst(e), x) - S.eval(S.snd(e), x)
-    elseif S.is_mul(e) then return S.eval(S.fst(e), x) * S.eval(S.snd(e), x)
-    elseif S.is_pow(e) then return S.eval(S.fst(e), x) ^ S.eval(S.snd(e), x) end
-end
-
-function S.is_empty(e)
-    return type(e) == "table" and #e == 0
-end
-
-function S.is_num(e)
-    return C.is_scalar(e)
-end
-
-function S.is_real(e)
-    return C.is_real(e)
-end
-
-function S.is_cpx(e)
-    return C.is_complex(e)
-end
-
-function S.is_var(e)
-    return type(e) == "string"
-end
-
-function S.is_add(e)
-    return type(e) == "table" and e[1] == "+"
-end
-
-function S.is_sub(e)
-    return type(e) == "table" and e[1] == "-"
-end
-
-function S.is_add_or_sub(e)
-    return S.is_add(e) or S.is_sub(e)
-end
-
-function S.is_mul(e)
-    return type(e) == "table" and e[1] == "*"
-end
-
-function S.is_pow(e)
-    return type(e) == "table" and e[1] == "^"
-end
-
-function S.is_bin(e)
-    return type(e) == "table" and (e[1] == "+" or e[1] == "-" or e[1] == "*" or e[1] == "^")
-end
-
-function S.op(e)
-    return e[1]
-end
-
-function S.fst(e)
-    return S(e[2])
-end
-
-function S.snd(e)
-    return S(e[3])
-end
-
-function S.add(e, f)
-    if S.is_empty(e) and S.is_empty(f) then return S()
-    elseif S.is_empty(e) and not S.is_empty(f) then return f
-    elseif not S.is_empty(e) and S.is_empty(f) then return e
-    else return S({"+", e, f}) end
-end
-
-S.mt.__add = S.add
-
-function S.mul(e, f)
-    if S.is_empty(e) and S.is_empty(f) then return S()
-    elseif S.is_empty(e) and not S.is_empty(f) then return f
-    elseif not S.is_empty(e) and S.is_empty(f) then return e
-    else return S({"*", e, f}) end
-end
-
-S.mt.__mul = S.mul
-
-function S.pow(e, n)
-    if S.is_empty(e) then return S()
-    else return S({"^", e, n}) end
-end
-
-S.mt.__pow = S.pow
-
-do
-    local f = S {"-", {"*", "x0", "x3"}, {"*", "x1", "x2"}}
-    local g = S {"-", {"*", "x0", "x2"}, {"*", "x1", "x1"}}
-    local h = S {"-", {"*", "x1", "x3"}, {"*", "x2", "x2"}}
-    local F = S.normal(f * f + g * g + h * h)
-    print(F)
-end
+-- example usage
+-- do
+--     local x0, x1, x2, x3 = S("x0"), S("x1"), S("x2"), S("x3")
+--     local f = x0*x3 - x1*x2
+--     local g = x0*x2 - x1*x1
+--     local h = x1*x3 - x2*x2
+--     local F = f^2 + g^2 + h^2
+--     print(F:normal())
+--     print(F:der(x0):normal())
+-- end
 
 return S
--- vector.lua
+++ vector.lua
@@ -14,15 +14,21 @@ function shallow_copy(orig)
     return copy
 end
 
-function deep_copy(orig)
+function deep_copy(orig, copies)
+    copies = copies or {}
     local orig_type = type(orig)
     local copy
     if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[deep_copy(orig_key)] = deep_copy(orig_value)
+        if copies[orig] then
+            copy = copies[orig]
+        else
+            copy = {}
+            copies[orig] = copy
+            for orig_key, orig_value in next, orig, nil do
+                copy[deep_copy(orig_key, copies)] = deep_copy(orig_value, copies)
+            end
+            setmetatable(copy, deep_copy(getmetatable(orig), copies))
         end
-        setmetatable(copy, deep_copy(getmetatable(orig)))
     else -- number, string, boolean, etc
         copy = orig
     end
